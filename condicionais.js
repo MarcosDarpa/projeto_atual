ee8e835e7578556a720423998019e296ef9bd924
MarcosDarpa <130925272+MarcosDarpa@users.noreply.github.com>
2023-10-18 10:12:17 -0300

    Create condicionais.js

---
+++ condicionais.js
@@ -0,0 +1,20 @@
+console.log(`Trabalhando com condicionais`);
+var destino = ['Salvador', ' São Paulo', ' Rio de Janeiro']; 
+console.log(`${destino}`);                        
+
+
+
+destino.push("Curitiba"); 
+
+const idadeComprador = 15
+console.log("Destinos possíveis:");
+console.log(destino);
+
+if (idadeComprador >= 18); {
+    console.log("Comprador maior de idade")
+}
+
+destino.splice(2, 1); 
+console.log(destino);
+
+console.log(destino[1]); 
